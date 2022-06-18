5fb34c1e0457cf04a2f44507cb512610a5d53574
Christopher Bohorquez <chrisishere4@outlook.com>
2022-06-17 20:36:49 -0400

    Navbar and Carousel added

--- pages/testImages.js
+++ pages/testImages.js
@@ -1,36 +1,36 @@
 import React from "react";
 import Image from "next/image";
 import buildRequest from "../utils/igdb/buildRequest";
-
-// import style from "../styles/Test.module.css";
+import Slider from "react-slick";
 
 function Test({ gameList }) {
   return (
     <div>
       <h1>Test Page</h1>
-      <div className="posterContainer">
-        <div className="poster">
-          {gameList.map((game) => {
-            // let imgSrc = "https:" + game.url.replace("t_thumb", "t_cover_big");
-            return (
-              <div key={game.id} className="box posterItem">
-                <figure className="image is-128x128 is-2by3">
-                  <Image
-                    // key={game.id}
-                    src={
-                      game.cover
-                        ? "https:" + game.cover.url.replace("t_thumb", "t_720p")
-                        : "https://bulma.io/images/placeholders/128x128.png"
-                    }
-                    layout="fill"
-                    objectFit="contain"
-                  />
-                </figure>
-              </div>
-            );
-          })}
+        <div className="posterContainer">
+          <div className="poster">
+            {gameList.map((game) => {
+              // let imgSrc = "https:" + game.url.replace("t_thumb", "t_cover_big");
+              return (
+                <div  className="box posterItem">
+                  <figure className="image is-128x128 is-2by3">
+                    <Image
+                      // key={game.id}
+                      src={
+                        game.cover
+                          ? "https:" + game.cover.url.replace("t_thumb", "t_720p")
+                          : "https://bulma.io/images/placeholders/128x128.png"
+                      }
+                      layout="fill"
+                      objectFit="contain"
+                    />
+                  </figure>
+                  <span className="gameName">{game.name}</span>
+                </div>
+              );
+            })}
+          </div>
         </div>
-      </div>
     </div>
   );
 }
