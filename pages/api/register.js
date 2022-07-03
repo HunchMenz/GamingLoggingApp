f0f8781e21de3cc19bacfa0a60079e85e4849719
Jhavaa <jhanava.r@gmail.com>
2022-07-03 17:56:59 -0400

    first commit of the 'add' api for user list.

--- pages/api/register.js
+++ pages/api/register.js
@@ -2,39 +2,46 @@ import bcrypt from "bcrypt";
 import Credentials from "../../database/user_data/model/Credentials";
 
 export default async function handler(req, res) {
-  const body = req.body;
+  if (req.method === "POST") {
+    const body = req.body;
 
-  for (const key in body) {
-    if (body[key] === "") {
-      return res
-        .status(400)
-        .json({ message: "Must fill out all required fields to register." });
+    for (const key in body) {
+      if (body[key] === "") {
+        return res
+          .status(400)
+          .json({ message: "Must fill out all required fields to register." });
+      }
     }
-  }
 
-  const query = {
-    email: body.email,
-  };
-  const userExist = await Credentials.findOne(query);
+    const query = {
+      email: body.email,
+    };
+    const userExist = await Credentials.findOne(query);
 
-  if (userExist) {
+    if (userExist) {
+      return res
+        .status(200)
+        .json({ message: "User already exists with submitted email." });
+    }
+
+    // generate salt to hash password
+    const salt = await bcrypt.genSalt(10);
+    // now we set user password to hashed password
+    const hashedPass = await bcrypt.hash(body.password, salt);
+
+    const newUser = {
+      username: body.username,
+      email: body.email,
+      password: hashedPass,
+    };
+    const user = new Credentials(newUser);
+    await user.save();
+
+    return res.status(200).json({ message: "Registered Successfully" });
+  } else
     return res
-      .status(200)
-      .json({ message: "User already exists with submitted email." });
-  }
-
-  // generate salt to hash password
-  const salt = await bcrypt.genSalt(10);
-  // now we set user password to hashed password
-  const hashedPass = await bcrypt.hash(body.password, salt);
-
-  const newUser = {
-    username: body.username,
-    email: body.email,
-    password: hashedPass,
-  };
-  const user = new Credentials(newUser);
-  await user.save();
-
-  return res.status(200).json({ message: "Registered Successfully" });
+      .status(401)
+      .json({
+        message: "Error in completing request: invalid request method.",
+      });
 }
