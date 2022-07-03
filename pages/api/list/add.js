fff352a83c38d293dc7ce2ec884b968d984f5e9a
Jhavaa <jhanava.r@gmail.com>
2022-07-03 05:53:15 -0400

    Made UI change to NavBar. Created (blank) user list page. Updated list api with TODO comments.

--- pages/api/list/add.js
+++ pages/api/list/add.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+// TODO: Write API that adds a game to a users game list */
 export default function handler(req, res) {
   res.status(200).json({ name: "John Doe" });
 }
--- pages/api/list/read.js
+++ pages/api/list/read.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-// TODO: Write API that updates a users game list. NOTE: Not delete or add. More like status updates on games (i.e. in progress, completed, backlog, etc.)
+// TODO: Write API that retrieve's a users game list
 export default function handler(req, res) {
   res.status(200).json({ name: "John Doe" });
 }
