cfe1b0907772664255a8b137dd3e9f6bf8962d44
Jhavaa <jhanava.r@gmail.com>
2022-07-03 05:53:15 -0400

    Made UI change to NavBar. Created (blank) user list page. Updated list api with TODO comments.

--- pages/api/list/remove.js
+++ pages/api/list/remove.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+// TODO: Write API that deletes game from a users game list */
 export default function handler(req, res) {
   res.status(200).json({ name: "John Doe" });
 }
