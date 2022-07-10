f6928b4d92aca1af91501659eeff5610632c7e47
Jhavaa <jhanava.r@gmail.com>
2022-07-09 20:03:13 -0400

    Styled Login and Register page so that the buttons are in a column and not next to eachother. Added google provider and 'profile' function to all providers. Changed database that user credentials are being saved to.

--- pages/api/auth/lib/mongodb.js
+++ pages/api/auth/lib/mongodb.js
@@ -1,7 +1,7 @@
 // This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI_USERDATA;
 const options = {
   useUnifiedTopology: true,
   useNewUrlParser: true,
@@ -10,7 +10,7 @@ const options = {
 let client;
 let clientPromise;
 
-if (!process.env.MONGODB_URI) {
+if (!process.env.MONGODB_URI_USERDATA) {
   throw new Error("Please add your Mongo URI to .env.local");
 }
 
