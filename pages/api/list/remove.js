b5fd97e0cd96e18b0b68dd3a1a0feaca3dc6d1dc
Jhavaa <jhanava.r@gmail.com>
2022-07-11 18:35:20 -0400

    Removed unnecessary api's

--- pages/api/list/remove.js
+++
@@ -1,7 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-// TODO: Write API that deletes game from a users game list
-export default function handler(req, res) {
-  // use array.filter(gameID) to remove specific id from array
-  res.status(200).json({ name: "John Doe" });
-}
