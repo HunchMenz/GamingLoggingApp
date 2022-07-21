29c53ac1a42908f0df13e3338c1fff62cbff4d0e
srayce1 <39781724+srayce1@users.noreply.github.com>
2022-07-21 12:57:44 -0500

    Cleaned up some syntax issues and removed comments

--- components/NavBar.js
+++ components/NavBar.js
@@ -25,44 +25,11 @@ function NavBar() {
           </Link>
         </div>
         <div className="flex-none">
-          {/* <div className="dropdown dropdown-end">
-          <label tabIndex="0" className="btn btn-ghost btn-circle">
-            <div className="indicator">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                />
-              </svg>
-              <span className="badge badge-sm indicator-item">8</span>
-            </div>
-          </label>
-          <div
-            tabIndex="0"
-            className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow"
-          >
-            <div className="card-body">
-              <span className="font-bold text-lg">8 Items</span>
-              <span className="text-info">Subtotal: $999</span>
-              <div className="card-actions">
-                <button className="btn btn-primary btn-block">View cart</button>
-              </div>
-            </div>
-          </div>
-        </div> */}
           <div className="flex-none">
             <ul className="menu menu-horizontal p-0 primary-focus">
               <li>
                 <select data-choose-theme>
-                  <option value="">default</option>
+                  <option seleced value>default</option>
                   {themeValues.map((value) => (
                     <option
                       className="text-primary"
