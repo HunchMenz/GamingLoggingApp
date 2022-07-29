706582bb5b4ba48cc0af77fa113d49416feb3bad
Jhavaa <jhanava.r@gmail.com>
2022-07-29 18:34:54 -0400

    Fixed missing navbar links. Toggled user icon. User profile pic used. fixed classname.

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,11 +1,12 @@
 import React, { useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { signIn, signOut } from "next-auth/react";
 import { themeChange } from "theme-change";
+import { useGameListContext } from "../context/gameList";
 
 function NavBar() {
-  const { data, status } = useSession();
+  const { user, gameList } = useGameListContext();
   const themeValues = ["cupcake", "cyberpunk", "light", "dark", "bumblebee"];
   useEffect(() => {
     themeChange(false);
@@ -15,18 +16,15 @@ function NavBar() {
       <div className="navbar bg-ghost">
         <div className="text-left">
           <Link href="/">
-            <a
-              className="btn bg-transparent border-none hover:bg-transparent normal-case text-5xl primary-content"
-              href="/"
-            >
-              <p class="text-highlight">G</p>
+            <a className="btn bg-transparent border-none hover:bg-transparent normal-case text-5xl primary-content">
+              <p className="text-highlight">G</p>
               <p className="text-primary">-List</p>
             </a>
           </Link>
         </div>
         <div className="flex-1">
           <div className="flex-1">
-            <ul className="menu menu-horizontal p-0 text-white text-xl font-bold">
+            <ul className="menu menu-horizontal p-0 text-xl font-bold">
               <li>
                 <Link href="/testImages">
                   <a className="bg-transparent hover:text-hovered focus:text-selected">
@@ -49,7 +47,7 @@ function NavBar() {
                 </Link>
               </li>
               <li>
-                <Link href="/">
+                <Link href="/user/list">
                   <a className="bg-transparent hover:text-hovered focus:text-selected">
                     Game List
                   </a>
@@ -73,30 +71,36 @@ function NavBar() {
               </li>
             </ul>
           </div>
-          <div className="dropdown dropdown-end text-white mr-5">
-            <label tabIndex="0" className="btn btn-ghost btn-circle avatar">
-              <div className="w-10 rounded-full">
-                <img src="https://placeimg.com/80/80/people" />
-              </div>
-            </label>
-            <ul
-              tabIndex="0"
-              className="menu dropdown-content shadow bg-dropdown object-cover divide-y divide-blue-900 divide-10"
+          {user ? (
+            <div className="dropdown dropdown-end text-white mr-5">
+              <label tabIndex="0" className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full">
+                  <img src={user.image} />
+                </div>
+              </label>
+              <ul
+                tabindex="0"
+                class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a>Profile</a>
+                </li>
+                <li>
+                  <a>Settings</a>
+                </li>
+                <li>
+                  <button onClick={() => signOut()}>Logout</button>
+                </li>
+              </ul>
+            </div>
+          ) : (
+            <button
+              className="bg-transparent hover:text-hovered focus:text-selected text-white text-xl font-bold px-4 py-3"
+              onClick={() => signIn()}
             >
-              <li>
-                <a className="bg-transparent hover:text-hovered">Profile</a>
-              </li>
-              <li>
-                <a className="bg-transparent hover:text-hovered">Support</a>
-              </li>
-              <li>
-                <a className="bg-transparent hover:text-hovered">Settings</a>
-              </li>
-              <li>
-                <a className="bg-transparent hover:text-hovered">Logout</a>
-              </li>
-            </ul>
-          </div>
+              Login
+            </button>
+          )}
         </div>
       </div>
     </>
