c5832f637e827c777ea1912577a0093d9ea5ec36
Christopher Bohorquez <chrisishere4@outlook.com>
2022-07-28 20:44:20 -0400

    Optimized genre queries

--- pages/testSlider.js
+++ pages/testSlider.js
@@ -2,13 +2,81 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import Slider from "../components/Slider";
 import buildRequest from "../utils/buildRequest";
+import requests from "../utils/requests";
 
-function myCarousel({ gameMasterList }) {
+function myCarousel({ gameMasterList, testList }) {
   return (
     <>
       <NavBar />
+
+      {/* {console.log(testList[0].gameList)} */}
+      {/* {console.log(testList[0].title)} */}
+
       <Slider gameProp={gameMasterList.gameList0} sliderTitle="Top Games" />
+      <Slider gameProp={testList[0].gameList} sliderTitle={testList[0].title} />
+      <Slider gameProp={testList[1].gameList} sliderTitle={testList[1].title} />
+      <Slider gameProp={testList[2].gameList} sliderTitle={testList[2].title} />
+      <Slider gameProp={testList[3].gameList} sliderTitle={testList[3].title} />
+      <Slider gameProp={testList[4].gameList} sliderTitle={testList[4].title} />
+      <Slider gameProp={testList[5].gameList} sliderTitle={testList[5].title} />
+      <Slider gameProp={testList[6].gameList} sliderTitle={testList[6].title} />
+      <Slider gameProp={testList[7].gameList} sliderTitle={testList[7].title} />
+      <Slider gameProp={testList[8].gameList} sliderTitle={testList[8].title} />
+      <Slider gameProp={testList[9].gameList} sliderTitle={testList[9].title} />
       <Slider
+        gameProp={testList[10].gameList}
+        sliderTitle={testList[10].title}
+      />
+      <Slider
+        gameProp={testList[11].gameList}
+        sliderTitle={testList[11].title}
+      />
+      <Slider
+        gameProp={testList[12].gameList}
+        sliderTitle={testList[12].title}
+      />
+      <Slider
+        gameProp={testList[13].gameList}
+        sliderTitle={testList[13].title}
+      />
+      <Slider
+        gameProp={testList[14].gameList}
+        sliderTitle={testList[14].title}
+      />
+      <Slider
+        gameProp={testList[15].gameList}
+        sliderTitle={testList[15].title}
+      />
+      <Slider
+        gameProp={testList[16].gameList}
+        sliderTitle={testList[16].title}
+      />
+      <Slider
+        gameProp={testList[17].gameList}
+        sliderTitle={testList[17].title}
+      />
+      <Slider
+        gameProp={testList[18].gameList}
+        sliderTitle={testList[18].title}
+      />
+      <Slider
+        gameProp={testList[19].gameList}
+        sliderTitle={testList[19].title}
+      />
+      <Slider
+        gameProp={testList[20].gameList}
+        sliderTitle={testList[20].title}
+      />
+      {/* <Slider
+        gameProp={testList[21].gameList}
+        sliderTitle={testList[21].title}
+      /> */}
+      {/* <Slider
+        gameProp={testList[22].gameList}
+        sliderTitle={testList[22].title}
+      /> */}
+
+      {/* <Slider
         gameProp={gameMasterList.gameList1}
         sliderTitle="Point and Click"
       />
@@ -17,9 +85,7 @@ function myCarousel({ gameMasterList }) {
       <Slider gameProp={gameMasterList.gameList4} sliderTitle="Music" />
       <Slider gameProp={gameMasterList.gameList5} sliderTitle="Platform" />
       <Slider gameProp={gameMasterList.gameList6} sliderTitle="Puzzle" />
-      <Slider gameProp={gameMasterList.gameList7} sliderTitle="Racing" />
-
-      {console.log(gameMasterList.gameList1)}
+      <Slider gameProp={gameMasterList.gameList7} sliderTitle="Racing" /> */}
     </>
   );
 }
@@ -45,54 +111,54 @@ export async function getServerSideProps() {
 
   const response_0 = await buildRequest("igdb", "games", query_0);
 
-  const filter_1 =
-    "sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (2); limit 20;";
+  // const filter_1 =
+  //   "sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (2); limit 20;";
 
-  const query_1 = "fields " + fields.join(",") + ";" + filter_1;
+  // const query_1 = "fields " + fields.join(",") + ";" + filter_1;
 
-  const response_1 = await buildRequest("igdb", "games", query_1);
+  // const response_1 = await buildRequest("igdb", "games", query_1);
 
-  const filter_2 =
-    "sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (4); limit 20;";
+  // const filter_2 =
+  //   "sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (4); limit 20;";
 
-  const query_2 = "fields " + fields.join(",") + ";" + filter_2;
+  // const query_2 = "fields " + fields.join(",") + ";" + filter_2;
 
-  const response_2 = await buildRequest("igdb", "games", query_2);
+  // const response_2 = await buildRequest("igdb", "games", query_2);
 
-  const filter_3 =
-    "sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (5); limit 20;";
+  // const filter_3 =
+  //   "sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (5); limit 20;";
 
-  const query_3 = "fields " + fields.join(",") + ";" + filter_3;
+  // const query_3 = "fields " + fields.join(",") + ";" + filter_3;
 
-  const response_3 = await buildRequest("igdb", "games", query_3);
+  // const response_3 = await buildRequest("igdb", "games", query_3);
 
-  const filter_4 =
-    "sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (7); limit 20;";
+  // const filter_4 =
+  //   "sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (7); limit 20;";
 
-  const query_4 = "fields " + fields.join(",") + ";" + filter_4;
+  // const query_4 = "fields " + fields.join(",") + ";" + filter_4;
 
-  const response_4 = await buildRequest("igdb", "games", query_4);
+  // const response_4 = await buildRequest("igdb", "games", query_4);
 
-  const filter_5 =
-    "sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (8); limit 20;";
+  // const filter_5 =
+  //   "sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (8); limit 20;";
 
-  const query_5 = "fields " + fields.join(",") + ";" + filter_5;
+  // const query_5 = "fields " + fields.join(",") + ";" + filter_5;
 
-  const response_5 = await buildRequest("igdb", "games", query_5);
+  // const response_5 = await buildRequest("igdb", "games", query_5);
 
-  const filter_6 =
-    "sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (9); limit 20;";
+  // const filter_6 =
+  //   "sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (9); limit 20;";
 
-  const query_6 = "fields " + fields.join(",") + ";" + filter_6;
+  // const query_6 = "fields " + fields.join(",") + ";" + filter_6;
 
-  const response_6 = await buildRequest("igdb", "games", query_6);
+  // const response_6 = await buildRequest("igdb", "games", query_6);
 
-  const filter_7 =
-    "sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (10); limit 20;";
+  // const filter_7 =
+  //   "sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (10); limit 20;";
 
-  const query_7 = "fields " + fields.join(",") + ";" + filter_7;
+  // const query_7 = "fields " + fields.join(",") + ";" + filter_7;
 
-  const response_7 = await buildRequest("igdb", "games", query_7);
+  // const response_7 = await buildRequest("igdb", "games", query_7);
 
   ///////////////////////////////////////////////////////////////////////////
 
@@ -121,18 +187,17 @@ export async function getServerSideProps() {
   ///////////////////////////////////////////////////////////////////////////
 
   const gameMasterList = {
-    genreList: genreResponse,
+    // genreList: genreResponse,
     gameList0: response_0,
-    gameList1: response_1,
-    gameList2: response_2,
-    gameList3: response_3,
-    gameList4: response_4,
-    gameList5: response_5,
-    gameList6: response_6,
-    gameList7: response_7,
+    // gameList1: response_1,
+    // gameList2: response_2,
+    // gameList3: response_3,
+    // gameList4: response_4,
+    // gameList5: response_5,
+    // gameList6: response_6,
+    // gameList7: response_7,
   };
 
-  /** Jhanava's Solution! (not doing your work chris) ** Make sure to import request **
   let p = [];
   let test = [];
 
@@ -141,23 +206,21 @@ export async function getServerSideProps() {
     test.push({ title: requests.home[listPromiseObj].title });
   }
 
-  console.log(p);
   const pResponse = await Promise.all(p);
 
   const test2 = pResponse.map((e, idx) => {
-    return { title: test[idx], gameList: e };
+    return { title: test[idx].title, gameList: e };
   });
 
-  console.log(test2);
-
   const response = test2;
-  return {
-    props: { gameList: response },
-  };
-*/
+  // return {
+  //   props: { gameList: response },
+  // };
+
   return {
     props: {
       gameMasterList: gameMasterList,
+      testList: response,
     },
   };
 }
--- utils/requests.js
+++ utils/requests.js
@@ -2,29 +2,189 @@ import buildRequest from "./buildRequest";
 // TODO: define each type of requests
 export default {
   home: {
-    Trending: {
-      title: "Trending",
+    PointandClick: {
+      title: "Point-and-Click",
       promise: buildRequest(
         "igdb",
         "games",
-        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90;"
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (2); limit 20;"
       ),
     },
-    Horror: {
-      title: "Horror",
+    Fighting: {
+      title: "Fighting",
       promise: buildRequest(
         "igdb",
         "games",
-        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90;"
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (4); limit 20;"
       ),
     },
+    Shooter: {
+      title: "Shooter",
+      promise: buildRequest(
+        "igdb",
+        "games",
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (5); limit 20;"
+      ),
+    },
+    Music: {
+      title: "Music",
+      promise: buildRequest(
+        "igdb",
+        "games",
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (7); limit 20;"
+      ),
+    },
+    Platform: {
+      title: "Platform",
+      promise: buildRequest(
+        "igdb",
+        "games",
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (8); limit 20;"
+      ),
+    },
+    Puzzel: {
+      title: "Puzzel",
+      promise: buildRequest(
+        "igdb",
+        "games",
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (9); limit 20;"
+      ),
+    },
+    Racing: {
+      title: "Racing",
+      promise: buildRequest(
+        "igdb",
+        "games",
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (10); limit 20;"
+      ),
+    },
+    RTS: {
+      title: "Real Time Strategy",
+      promise: buildRequest(
+        "igdb",
+        "games",
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (11); limit 20;"
+      ),
+    },
+    RPG: {
+      title: "Role Playing Game",
+      promise: buildRequest(
+        "igdb",
+        "games",
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (12); limit 20;"
+      ),
+    },
+    Simulator: {
+      title: "Simulator",
+      promise: buildRequest(
+        "igdb",
+        "games",
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (13); limit 20;"
+      ),
+    },
+    Sport: {
+      title: "Sport",
+      promise: buildRequest(
+        "igdb",
+        "games",
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (14); limit 20;"
+      ),
+    },
+    Strategy: {
+      title: "Strategy",
+      promise: buildRequest(
+        "igdb",
+        "games",
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (15); limit 20;"
+      ),
+    },
+    TurnBased: {
+      title: "Turn-Based Strategy",
+      promise: buildRequest(
+        "igdb",
+        "games",
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (16); limit 20;"
+      ),
+    },
+    Tactical: {
+      title: "Tactical",
+      promise: buildRequest(
+        "igdb",
+        "games",
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (24); limit 20;"
+      ),
+    },
+    QuizTrivia: {
+      title: "Quiz/Trivia",
+      promise: buildRequest(
+        "igdb",
+        "games",
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (26); limit 20;"
+      ),
+    },
+    HackandSlash: {
+      title: "Hack and Slash",
+      promise: buildRequest(
+        "igdb",
+        "games",
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (25); limit 20;"
+      ),
+    },
+    // Pinball: {
+    //   title: "Pinball",
+    //   promise: buildRequest(
+    //     "igdb",
+    //     "games",
+    //     "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (30); limit 20;"
+    //   ),
+    // },
     Adventure: {
       title: "Adventure",
       promise: buildRequest(
         "igdb",
         "games",
-        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90;"
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (31); limit 20;"
       ),
     },
+    Arcade: {
+      title: "Arcade",
+      promise: buildRequest(
+        "igdb",
+        "games",
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (33); limit 20;"
+      ),
+    },
+    VisualNovel: {
+      title: "Visual Novel",
+      promise: buildRequest(
+        "igdb",
+        "games",
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (34); limit 20;"
+      ),
+    },
+    Indie: {
+      title: "Indie",
+      promise: buildRequest(
+        "igdb",
+        "games",
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (32); limit 20;"
+      ),
+    },
+    CardBoard: {
+      title: "Card and Board Games",
+      promise: buildRequest(
+        "igdb",
+        "games",
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (35); limit 20;"
+      ),
+    },
+    // MOBA: {
+    //   title: "MOBA",
+    //   promise: buildRequest(
+    //     "igdb",
+    //     "games",
+    //     "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (36); limit 20;"
+    //   ),
+    // },
   },
 };
