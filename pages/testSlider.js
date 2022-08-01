44f243c565e46db077f3dd3b99075f60b1a73f6f
Jhavaa <jhanava.r@gmail.com>
2022-08-01 01:56:19 -0400

    Found 'MultiQuery' as an option, so def gonna use that.

--- pages/testSlider.js
+++ pages/testSlider.js
@@ -2,25 +2,25 @@ import React from "react";
 import FeaturedSlider from "../components/FeaturedSlider";
 import NavBar from "../components/NavBar";
 import Slider from "../components/Slider";
+import buildRequest from "../utils/buildRequest";
 import requests from "../utils/requests";
 
 function myCarousel({ gameMasterList }) {
   return (
     <>
       <NavBar />
-      <FeaturedSlider gameProp={gameMasterList[0].gameList} />
       {gameMasterList.map((obj) => {
-        return obj.id === 0 ? (
+        return obj.name === "Top Games" ? (
           <FeaturedSlider
             key={`slider-genre-${obj.id}`}
-            gameProp={obj.gameList}
-            sliderTitle={obj.title}
+            gameProp={obj.result}
+            sliderTitle={obj.name}
           />
         ) : (
           <Slider
             key={`slider-genre-${obj.id}`}
-            gameProp={obj.gameList}
-            sliderTitle={obj.title}
+            gameProp={obj.result}
+            sliderTitle={obj.name}
           />
         );
       })}
@@ -29,33 +29,48 @@ function myCarousel({ gameMasterList }) {
 }
 
 export async function getServerSideProps() {
-  // Array of Promises
-  let p = [];
-  // Array of Carousel data objects
-  let data = [];
+  // // Array of Promises
+  // let p = [];
+  // // Array of Carousel data objects
+  // let data = [];
 
-  // Loop through all object requests under home.
-  for (const listPromiseObj in requests.home) {
-    // Save each promise to the promise array
-    p.push(requests.home[listPromiseObj].promise);
-    // Add Carousel Title to Titles array
-    data.push({
-      id: requests.home[listPromiseObj].id,
-      title: requests.home[listPromiseObj].title,
-    });
+  // // Loop through all object requests under home.
+  // for (const listPromiseObj in requests.home) {
+  //   // Save each promise to the promise array
+  //   p.push(requests.home[listPromiseObj].promise);
+  //   // Add Carousel Title to Titles array
+  //   data.push({
+  //     id: requests.home[listPromiseObj].id,
+  //     title: requests.home[listPromiseObj].title,
+  //   });
+  // }
+
+  // // Resolve all Promises in Promise array in parallel
+  // const pResponse = await Promise.all(p);
+
+  // // Create new Array of Carousel data objects
+  // const carousels = pResponse.map((list, idx) => {
+  //   return { id: data[idx].id, title: data[idx].title, gameList: list };
+  // });
+
+  let multiQueryBody = "";
+  let i = 0;
+  for (const listPromiseObj in requests.home2) {
+    i++;
+    multiQueryBody = multiQueryBody.concat(
+      `query games "${requests.home2[listPromiseObj].title}" { ${requests.home2[listPromiseObj].query} };\n`
+    );
+
+    if (i == 10) {
+      break;
+    }
   }
 
-  // Resolve all Promises in Promise array in parallel
-  const pResponse = await Promise.all(p);
-
-  // Create new Array of Carousel data objects
-  const carousels = pResponse.map((list, idx) => {
-    return { id: data[idx].id, title: data[idx].title, gameList: list };
-  });
+  const response = await buildRequest("igdb", "multiquery", multiQueryBody);
 
   return {
     props: {
-      gameMasterList: carousels,
+      gameMasterList: response,
     },
   };
 }
--- utils/requests.js
+++ utils/requests.js
@@ -219,4 +219,160 @@ export default {
     //   ),
     // },
   },
+
+  home2: {
+    TopGames: {
+      id: 0,
+      title: "Top Games",
+      query:
+        "fields name, slug, cover.url, screenshots.url, rating, summary; sort aggregated_rating_count desc; where aggregated_rating >= 90; limit 20;",
+    },
+    PointandClick: {
+      id: 2,
+      title: "Point-and-Click",
+      query:
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (2); limit 20;",
+    },
+    Fighting: {
+      id: 4,
+      title: "Fighting",
+      query:
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (4); limit 20;",
+    },
+    Shooter: {
+      id: 5,
+      title: "Shooter",
+      query:
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (5); limit 20;",
+    },
+    Music: {
+      id: 7,
+      title: "Music",
+      query:
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (7); limit 20;",
+    },
+    Platform: {
+      id: 8,
+      title: "Platform",
+      query:
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (8); limit 20;",
+    },
+    Puzzle: {
+      id: 9,
+      title: "Puzzle",
+      query:
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (9); limit 20;",
+    },
+    Racing: {
+      id: 10,
+      title: "Racing",
+      query:
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (10); limit 20;",
+    },
+    RTS: {
+      id: 11,
+      title: "Real Time Strategy",
+      query:
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (11); limit 20;",
+    },
+    RPG: {
+      id: 12,
+      title: "Role Playing Game",
+      query:
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (12); limit 20;",
+    },
+    Simulator: {
+      id: 13,
+      title: "Simulator",
+      query:
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (13); limit 20;",
+    },
+    Sport: {
+      id: 14,
+      title: "Sport",
+      query:
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (14); limit 20;",
+    },
+    Strategy: {
+      id: 15,
+      title: "Strategy",
+      query:
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (15); limit 20;",
+    },
+    TurnBased: {
+      id: 16,
+      title: "Turn-Based Strategy",
+      query:
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (16); limit 20;",
+    },
+    Tactical: {
+      id: 24,
+      title: "Tactical",
+      query:
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (24); limit 20;",
+    },
+    // QuizTrivia: {
+    //   id: 26,
+    //   title: "Quiz/Trivia",
+    //   query: buildRequest(
+    //     "igdb",
+    //     "games",
+    //     "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (26); limit 20;"
+    //   ),
+    // },
+    HackandSlash: {
+      id: 25,
+      title: "Hack and Slash",
+      query:
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (25); limit 20;",
+    },
+    // Pinball: {
+    //   id: 30,
+    //   title: "Pinball",
+    //   query: buildRequest(
+    //     "igdb",
+    //     "games",
+    //     "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (30); limit 20;"
+    //   ),
+    // },
+    Adventure: {
+      id: 31,
+      title: "Adventure",
+      query:
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (31); limit 20;",
+    },
+    Arcade: {
+      id: 33,
+      title: "Arcade",
+      query:
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (33); limit 20;",
+    },
+    VisualNovel: {
+      id: 34,
+      title: "Visual Novel",
+      query:
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (34); limit 20;",
+    },
+    Indie: {
+      id: 32,
+      title: "Indie",
+      query:
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (32); limit 20;",
+    },
+    CardBoard: {
+      id: 35,
+      title: "Card and Board Games",
+      query:
+        "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (35); limit 20;",
+    },
+    // MOBA: {
+    //   id: 36,
+    //   title: "MOBA",
+    //   query: buildRequest(
+    //     "igdb",
+    //     "games",
+    //     "fields name, slug, cover.url; sort aggregated_rating_count desc; where aggregated_rating >= 90 & genres = (36); limit 20;"
+    //   ),
+    // },
+  },
 };
